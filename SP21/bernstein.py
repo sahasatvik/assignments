4c15ff7377ad39c7df3580eb709e0ea4c02e8b32
Satvik Saha <sahasatvik@gmail.com>
2021-07-15 17:38:10 +0530

    Added interactive slider

--- SP21/bernstein.py
+++ SP21/bernstein.py
@@ -5,6 +5,10 @@ import numpy as np
 
 @cache
 def binom(n, k):
+    '''
+    Calculates the binomial coefficient n choose k.
+    '''
+
     if k > n / 2:
         return binom(n, n - k)
     if k == 0:
@@ -12,24 +16,48 @@ def binom(n, k):
     return binom(n - 1, k - 1) * n // k
 
 def bernstein_poly(n, k):
+    '''
+    Returns the k'th Bernstein polynomial of degree n.
+    '''
+
     def p(x):
         return binom(n, k) * x**k * (1 - x)**(n - k)
     return p
 
 def bernstein(f, n):
+    '''
+    Returns the n'th degree Bernstein expansion of the function f.
+    '''
+
     def p(x):
         terms = [bernstein_poly(n, k)(x) * f(k / n) for k in range(0, n + 1)]
         return np.sum(terms, axis=0)
     return p
 
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    DEGREE = 8
-    colors = plt.get_cmap('Blues')(np.linspace(0.2, 1, DEGREE + 1))
+    from matplotlib.widgets import Slider
+
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.15)
+    fig.set_size_inches(10, 8)
+
+    degree_ax = plt.axes([0.2, 0.05, 0.65, 0.03])
+    degree_slider = Slider(degree_ax, 'Degree', 0, valmax=20, valinit=3, valstep=1)
     
     x = np.linspace(0, 1, 100)
-    for k, c in zip(range(DEGREE + 1), colors):
-        plt.plot(x, bernstein_poly(DEGREE, k)(x), color=c)
-    plt.title(f"Bernstein polynomials of degree {DEGREE}")
-    plt.xlim(0, 1)
+
+    def update(degree):
+        degree = int(degree)
+        colors = plt.get_cmap('Blues')(np.linspace(0.2, 1, degree + 1))
+        ax.cla()
+        ax.set_xlim(0, 1)
+        ax.set_title(f"Bernstein polynomials of degree {degree}")
+        for k, c in zip(range(degree + 1), colors):
+            ax.plot(x, bernstein_poly(degree, k)(x), color=c)
+
+    degree_slider.on_changed(update)
+    update(3)
+
     plt.show()
--- SP21/expansions.py
+++ SP21/expansions.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 
 from bernstein import bernstein
 
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.15)
+fig.set_size_inches(10, 8)
+
 def f(x):
     return np.cos(2 * np.pi * x) * np.exp(x)
 
@@ -14,7 +18,9 @@ colors = plt.get_cmap('Greys')(np.linspace(0, 0.9, MAX_DEGREE) ** 0.3)
 x = np.linspace(0, 1, 100)
 for n, c in zip(range(1, MAX_DEGREE), colors):
     plt.plot(x, bernstein(f, n)(x), color=c)
+
 plt.plot(x, f(x), '-r', linewidth=3)
+
 plt.title("Bernstein expansions of $e^x\cos(2\pi x)$")
 plt.xlim(0, 1)
 plt.show()
